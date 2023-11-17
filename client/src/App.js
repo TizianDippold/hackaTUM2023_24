ad0157c76a4851fcd94604b24c913f8d12569535
Lisa Schulze-Bergkamen <lisa.schulze-bergkamen@tum.de>
2023-11-18 00:47:49 +0100

    add initial chatbot functionality

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'react-chatbot-kit/build/main.css';
+import ChatbotComponent from "./components/ChatbotComponent";
+
+function App() {
+    return (
+        <div className="App">
+            <ChatbotComponent/>
+        </div>
+    );
+}
+
+export default App;
