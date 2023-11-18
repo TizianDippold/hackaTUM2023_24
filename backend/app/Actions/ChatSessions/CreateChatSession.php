8e846d1e9f3fdd78682a499dfb4d29dd48d92bf5
Moritz Maier <mor.maier@icloud.com>
2023-11-19 00:04:15 +0100

    create chat session using some other way

--- backend/app/Actions/ChatSessions/CreateChatSession.php
+++ backend/app/Actions/ChatSessions/CreateChatSession.php
@@ -13,10 +13,10 @@ class CreateChatSession
 
     public function create(): ChatSession
     {
-        $chatSession = ChatSession::create([
-            // Workaround so that we can have an empty object (not list!) in the database by default
-            'filter' => json_decode('{}'),
-        ]);
+        $chatSession = new ChatSession();
+        // Workaround so that we can have an empty object (not list!) in the database by default
+        $chatSession->filter = json_decode('{}');
+        $chatSession->save();
         // Load database default values
         $chatSession->refresh();
         $this->systemPromptCreator->create($chatSession);
