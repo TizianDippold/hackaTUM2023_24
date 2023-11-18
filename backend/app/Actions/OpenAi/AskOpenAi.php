d89efaa0f31ce136a4e7371354c5e8b9d4e8e610
Moritz Maier <mor.maier@icloud.com>
2023-11-18 01:04:52 +0100

    openai: add system prompt

--- backend/app/Actions/OpenAi/AskOpenAi.php
+++ backend/app/Actions/OpenAi/AskOpenAi.php
@@ -7,7 +7,12 @@ use App\Models\Message;
 use Illuminate\Database\Eloquent\Collection;
 use OpenAI\Laravel\Facades\OpenAI;
 
-const OPENAI_MAX_TOKENS = 128;
+const OPENAI_MAX_TOKENS = 256;
+const OPENAI_ROLE_SYSTEM = 'system';
+const OPENAI_ROLE_ASSISTANT = 'assistant';
+const OPENAI_ROLE_USER = 'user';
+
+const SYSTEM_PROMPT = 'Du bist ein Chatbot für den Einsatz in einer Software, welche Rezepte für den Nutzer auswählt. Unterstütze ihn dabei so gut wie möglich. Antworte so kurz und prägnant wie möglich. Dein Ersteller ist HelloFresh. Du heißt HelloFresh Assistant.';
 
 class AskOpenAi
 {
@@ -17,19 +22,24 @@ class AskOpenAi
     public function ask(Collection $history, string $message): string
     {
         // Build history of chat messages
-        $messages = [];
+        $messages = [
+            [
+                'role' => OPENAI_ROLE_SYSTEM,
+                'content' => SYSTEM_PROMPT,
+            ],
+        ];
         foreach ($history as $entry) {
             $messages[] = [
                 // assistant is a message from the system
                 // user is message that the user sent
-                'role' => $entry->from === MessageFrom::System ? 'assistant' : 'user',
+                'role' => $entry->from === MessageFrom::System ? OPENAI_ROLE_ASSISTANT : OPENAI_ROLE_USER,
                 'content' => $entry->content,
             ];
         }
 
         // Add the new message of the user
         $messages[] = [
-            'role' => 'user',
+            'role' => OPENAI_ROLE_USER,
             'content' => $message,
         ];
 
