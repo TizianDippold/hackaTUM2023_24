7d7999a716e3f5d29f582ed7662cfd980b5229b7
Lisa Schulze-Bergkamen <lisa.schulze-bergkamen@tum.de>
2023-11-19 01:05:21 +0100

    conditional text based on if recipes are showing

--- client/src/components/recipeResults.js
+++ client/src/components/recipeResults.js
@@ -5,7 +5,8 @@ import BigCard from "./bigCard";
 
 const RecipeResults = () => {
     const [recipes, setRecipes] = useState([]);
-
+    const [loading, setLoading] = useState(true);
+    const [found, setFound] = useState(false);
     const url = 'https://hackatum23.moremaier.com/api'
 
     const fetchChatResults = async () => {
@@ -31,7 +32,10 @@ const RecipeResults = () => {
         const fetchData = async () => {
             const allRecipes = await fetchChatResults();
             allRecipes.splice(3);
-            setRecipes(allRecipes);
+            setRecipes(prevState => allRecipes);
+            setLoading(false);
+            const foundRecipes = (allRecipes.length > 0);
+            setFound(foundRecipes);
         };
         fetchData();
     }, []);
@@ -55,8 +59,9 @@ const RecipeResults = () => {
                     src="https://img.hellofresh.com/f_auto,fl_lossy,q_auto/hellofresh_website/us/landing-pages/b2b/Hello_Fresh_White_Lockup_CMYK.png"
                     className="rounded-sm object-cover h-31 mt-10 "/>
 
-                <span className="mt-10 font-bold text-xl text-active">
-                    Here are your recommendations</span>
+                <span className="mt-10 font-bold text-xl text-active"> {
+                    loading ? 'Loading...' : found ? 'Here are some recipes!' : 'Sorry, no recipes found!'
+                }</span>
             </div>
             <div className="grid grid-cols-1 gap-5 p-5">
                 {recipes && recipes.map(recipe => (
